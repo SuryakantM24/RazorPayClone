3eed9c20a692c2328468ec39351401f3dd3d60a9
Suryakant Mudke <suryakantmudke1234@gmail.com>
2023-09-11 21:51:49 +0530

    Nav bar completed

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      fontFamily :{
+        mullish:["Mulish", "sans-serif"],
+      }
+      ,
+      colors:{
+         deepBlue: "#02042a",
+         lightBlue: "#2b84ea",
+         lightBlue300: "#4b94ed", 
+         lightBlue500: "#0b727", 
+         greenLight: "#61cea6",
+         grayText: "#818597", 
+         lightGray: "#e2e2e2", 
+         deepBlueHead : "#162f56",
+         gray2: "#525a76",
+         gratBlue:"#344a6c",
+         start :"#0070D1",
+         end:"#AB5C8B"
+      }
+    },
+  },
+  plugins: [],
+}
+
